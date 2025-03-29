bb5e81ae58de2026a610dfe0e36e930bffc49722
Viktor Login <batazor111@gmail.com>
2025-03-29 23:35:42 +0800

    refactor: update alliance data structure and add new YAML configurations

--- internal/domain/state.go
+++ internal/domain/state.go
@@ -72,34 +72,6 @@ type MessageStatus struct {
 	IsNewReports bool `yaml:"isNewReports"` // Признак наличия новых отчётов.
 }
 
-// Alliance описывает данные об альянсе, к которому принадлежит персонаж.
-type Alliance struct {
-	Name    string        `yaml:"name"`    // Название альянса.
-	Power   int           `yaml:"power"`   // Мощь альянса.
-	Members MembersInfo   `yaml:"members"` // Информация о членах альянса.
-	State   AllianceState `yaml:"state"`   // Дополнительное состояние альянса.
-	Tech    AllianceTech  `yaml:"tech"`    // Технологии альянса (например, вклад).
-}
-
-// MembersInfo содержит сведения о количестве участников альянса.
-type MembersInfo struct {
-	Count int `yaml:"count"` // Текущее количество участников.
-	Max   int `yaml:"max"`   // Максимальное количество участников.
-}
-
-// AllianceState описывает состояние альянса.
-type AllianceState struct {
-	IsNeedSupport              bool `yaml:"isNeedSupport"`              // Признак участия в альянсе.
-	IsWar                      int  `yaml:"isWar"`                      // Количество текущих войн.
-	IsChests                   int  `yaml:"isChests"`                   // Количество доступных сундуков.
-	IsAllianceContributeButton bool `yaml:"isAllianceContributeButton"` // Кнопка вклада в технологию
-}
-
-// AllianceTech описывает технологические аспекты альянса.
-type AllianceTech struct {
-	Favorite bool `yaml:"favorite"`
-}
-
 // Buildings представляет собой набор зданий персонажа.
 type Buildings struct {
 	Furnace Building `yaml:"furnace"` // Печь.
