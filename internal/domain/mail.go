5f1718c1e80c5514d662322bcbd0dcb3d2f53e7a
Viktor Login <batazor111@gmail.com>
2025-05-01 18:23:13 +0700

    feat: add mail check functionality with color check and update Mail struct

--- internal/domain/mail.go
+++ internal/domain/mail.go
@@ -1,8 +1,10 @@
 package domain
 
 type Mail struct {
+	IsHasMail bool `yaml:"is_has_mail"` // Флаг, указывающий, что есть непрочтенные письма.
+
 	IsWars     bool `yaml:"is_wars"`     // Флаг, указывающий, что почта содержит информацию о войне.
-	IsAlliance bool `yaml:"is_alliance"` // Флаг, указывающий, что почта содержит информацию о альянсе.
+	IsAlliance bool `yaml:"is_alliance"` // Флаг, указывающий, что почта содержит информацию об альянсе.
 	IsSystem   bool `yaml:"is_system"`   // Флаг, указывающий, что почта содержит системные сообщения.
 	IsReports  bool `yaml:"is_reports"`  // Флаг, указывающий, что почта содержит отчеты.
 }
