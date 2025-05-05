2746d82cea3922976399d437975bdb83d4bf7fb1
Viktor Login <batazor111@gmail.com>
2025-05-05 20:50:02 +0800

    feat: add welcome screen state management and related configurations

--- internal/domain/gamerState.go
+++ internal/domain/gamerState.go
@@ -2,4 +2,5 @@ package domain
 
 type ScreenState struct {
 	IsMainMenu bool `yaml:"isMainMenu"` // Флаг, указывающий, есть ли события в главном меню.
+	IsWelcome  bool `yaml:"isWelcome"`  // Флаг, указывающий, есть ли события приглашения новых выживших.
 }
